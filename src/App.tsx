4050c5ce6a6719fb9d3d58b9cd4f76ac78c5ff8a
Dan Mark <danmvillaplana@gmail.com>
2023-04-20 00:46:30 +0800

    Creating a component title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ReminderList from "./components/ReminderList";
+import Reminder from "./models/reminder";
+
+const reminders: Reminder[] = [{ id: 1, title: "Reminder 1" }];
 
 function App() {
     return (
         <div className="App">
-            <button className="btn btn-primary">Click Me</button>
+            <ReminderList items={reminders} />
         </div>
     );
 }
