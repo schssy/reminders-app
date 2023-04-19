873377228a9a23872fa894db08ae624446ada9e9
Dan Mark <danmvillaplana@gmail.com>
2023-04-20 03:07:55 +0800

    Using the Effect Hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
+import ReminderService from "./services/reminder";
 
 // const reminders: Reminder[] = [];
 
 function App() {
-    const [reminders, setReminders] = useState<Reminder[]>([{ id: 1, title: "Reminder 1" }]);
+    const [reminders, setReminders] = useState<Reminder[]>([]);
+
+    useEffect(() => {
+        loadReminders();
+    }, []);
+
+    const loadReminders = async () => {
+        const reminders = await ReminderService.getReminders();
+        setReminders(reminders);
+    };
 
     return (
         <div className="App">
