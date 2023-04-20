b42f6f9c9916bde6931a39fdb87c0b77a33af8f0
Dan Mark <danmvillaplana@gmail.com>
2023-04-21 06:15:35 +0800

    Building a form.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 import ReminderService from "./services/reminder";
+import NewReminder from "./components/NewReminder";
 
 // const reminders: Reminder[] = [];
 
@@ -25,6 +26,7 @@ function App() {
 
     return (
         <div className="App">
+            <NewReminder />
             <ReminderList items={reminders} onRemoveReminder={removeReminder} />
         </div>
     );
